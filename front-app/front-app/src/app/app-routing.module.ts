e6221046be03b352b254f221e36e29fef38a08f3
tcxtc00 <tanjaney0502@gmail.com>
2022-11-01 19:00:21 +0100

    front app created

---
+++ front-app/front-app/src/app/app-routing.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomepageComponent } from './homepage/homepage.component';
+import { RegistrationComponent } from './registration/registration.component';
+
+const routes: Routes = [
+  {path: 'registration', component: RegistrationComponent},
+  {path: '', component: HomepageComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
