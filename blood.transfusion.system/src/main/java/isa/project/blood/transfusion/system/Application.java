d2e49bba349b1069f8b287fe64e532882f2710be
tcxtc00 <tanjaney0502@gmail.com>
2022-11-01 14:59:47 +0100

    initial project created

---
+++ blood.transfusion.system/src/main/java/isa/project/blood/transfusion/system/Application.java
@@ -0,0 +1,13 @@
+package isa.project.blood.transfusion.system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
